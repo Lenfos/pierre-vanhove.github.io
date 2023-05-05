78c6a3488049dc96c84362db0ffea6a42533ad0b
Lenfos <pierre7159@gmail.com>
2023-05-05 13:52:11 +0200

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+let openDiv = false;
+
+let divToOpen = document.querySelector('.projectDiv');
+divToOpen.addEventListener()
+function showText(){
+    if (this.onclick === true && openDiv === false){
+        openDiv = true;
+        this.style.overflow = 'visible';
+        this.style.height = 'auto';
+        console.log("click");
+    }
+
+}
