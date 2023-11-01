f948e1da103b708f689e01c6229fd6eb203258a9
Pierre Vanhove <pierre.vanhove@iut-dijon.u-bourgogne.fr>
2023-11-01 13:15:53 +0100

    Mobile version v2

--- skillCard.js
+++ skillCard.js
@@ -1,16 +1,30 @@
 $(document).ready(function (){
     let card = $('.skillCard');
+    let width = $(window).width();
 
     card.click(function (){
-        if ($(this).hasClass('active')){
-            card.removeClass('active');
-            card.removeClass('other');
+        if (width > 576){
+            if ($(this).hasClass('active')){
+                card.removeClass('active');
+                card.removeClass('other');
+            }
+            else{
+                card.removeClass('active');
+                card.addClass('other');
+                $(this).removeClass('other');
+                $(this).toggleClass('active');
+            }
         }
-        else{
-            card.removeClass('active');
-            card.addClass('other');
-            $(this).removeClass('other');
-            $(this).toggleClass('active');
+        else {
+            if ($(this).hasClass('active')) {
+                card.removeClass('active');
+            }
+            else {
+                card.removeClass('active');
+                $(this).toggleClass('active');
+            }
+
         }
+
     })
 })
