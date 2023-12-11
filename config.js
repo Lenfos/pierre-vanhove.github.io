1003221fd4156713ab6f8ea97170a149be3485ef
Pierre Vanhove <pierre.vanhove@iut-dijon.u-bourgogne.fr>
2023-12-11 17:41:44 +0100

    Ajout de firebase au projet

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAkv1DTT5_pmouNxY_yN_-eWI8UwoH0BLI",
+    authDomain: "portfolio-e3341.firebaseapp.com",
+    projectId: "portfolio-e3341",
+    storageBucket: "portfolio-e3341.appspot.com",
+    messagingSenderId: "995500208846",
+    appId: "1:995500208846:web:9981a9857555c69617969f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
