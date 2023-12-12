11024f56c6be8b503d68091bbebd0c496579e879
Pierre Vanhove <pierre.vanhove@iut-dijon.u-bourgogne.fr>
2023-12-12 17:31:45 +0100

    Stop du caroussel lors du clique et essaie du chargement du fichier pour chaque clique

--- public/carroussel.js
+++ public/carroussel.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     const initalRotationSpeed = 3500;
-    const openRotationSpeed = 350000;
     const windowHeight = $(window).height();
     let card = $('.cardProject');
     let width = card.outerWidth(true);
@@ -8,9 +7,14 @@ $(document).ready(function(){
     let detail = $('#ProjectDetail');
     let close = $('#close');
     let topPos;
-    let open = false;
     const detailHeight = detail.height();
+    let interval = setInterval(Interval, initalRotationSpeed);
 
+    function Interval(){
+        $(".slider ul li:first-child").animate({"margin-left": (width + 42) * -1}, 1200, function(){
+            $(this).css("margin-left",20).appendTo(".slider ul");
+        });
+    }
 
     $(window).on("scroll", function () {
         const viewportPos = $(window).scrollTop();
@@ -21,7 +25,7 @@ $(document).ready(function(){
         detail.css('display', "none");
         $('body').css("overflow-y", "initial");
         $("section").css("filter", "none");
-        open = false;
+        interval = setInterval(Interval, initalRotationSpeed);
     });
 
     button.click(function (){
@@ -33,15 +37,9 @@ $(document).ready(function(){
         });
         $('body').css("overflow-y", "hidden");
         $("section").css("filter", "blur(10px)");
-        open = true;
+        let id = this.getAttribute('id');
+        $("#textProjectDetail").load(`Assets/TextProject/${id}.txt`);
+        clearInterval(interval);
     });
-
-    setInterval(function(){
-        $(".slider ul li:first-child").animate({"margin-left": (width + 42) * -1}, 1200, function(){
-            $(this).css("margin-left",20).appendTo(".slider ul");
-        });
-    }, open ? openRotationSpeed : initalRotationSpeed);
-
-
 })
 
